4357ff20d2cb86218af669ea915499537605c1fb
Sudhanshu <ersshekhar05@gmail.com>
2023-12-06 17:08:13 +0530

    updated code

--- app/info/page.js
+++ app/info/page.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 const Info = () => {
@@ -5,13 +6,18 @@ const Info = () => {
 
     return (
         <div id="info-page">
-
-
             <h1> Info page </h1>
             <h2>I am born in India</h2>
             <h2>and I like to play Cricket </h2>
+
+
+            <Link href="/blog">
              <button id="info-to-blog"> Go to Blog</button>
+             </Link>
+
+             <Link href="/">
             <button id="info-to-home"> Go to Home</button>
+            </Link>
         </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,15 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
 const Home = () => {
 
     return (
         <div id='home-page'>
-
             <h1>Home page</h1>
             <h2>Hi, Myself Developer </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
 
+            <Link href="/blog">
+                <button id="home-to-blog"> Go to Blog</button>
+            </Link>
+
+           <Link href= "/info">
+            <button id="home-to-info"> Go to Info</button>
+            </Link>
         </div>
     )
 }
